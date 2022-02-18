31b0b8eb058b1adb4356c235891c0c39e8998a15
ysun822 <sunyilin@seas.upenn.edu>
2022-02-18 09:22:07 -0800

    modify file for test

--- R/convert_currency.r
+++ R/convert_currency.r
@@ -1,23 +1,23 @@
-#' Get currency exchange rate between two currencies input by the user.
-#' 
-#' 
+#' @name convert_currency
+#'
+#' @title Convert Two Currencies Exchange Rate
+#'
 #' @details This function need the user input the currency to change from and the currency to change to.
 #'
 #' @param from String with the currency to change from. The format should be "XXX", for example "USD"
-#' 
+#'
 #' @param to  String with the currency to change to. The format should be "XXX", for example "CAD"
-#' 
-#' 
-#' @return It will return a character vector of length two. The first element is the exchange rate. The second element is the date.  
-#' If there is an error, it will return error and get the error message. 
-#' 
-#' @return the column will be the specific date currency exchange rate and the row will be the aim currency for that exchange rate. 
+#'
+#'
+#' @return It will return a character vector of length two. The first element is the exchange rate. The second element is the date.
+#' If there is an error, it will return error and get the error message.
+#'
+#' @return the column will be the specific date currency exchange rate and the row will be the aim currency for that exchange rate.
 #' @examples \dontrun{
 #' convert_currency("CAD", "USD")
 #' }
 #' @references \url{https://exchangerate.host/#/}
-options(warn=-1)
-install.packages("jsonlite",repos = "http://cran.us.r-project.org")
+
 library(jsonlite)
 currency_code_vector<-c('AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN',
                         'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BTC', 'BTN', 'BWP', 'BYN', 'BZD',
@@ -26,29 +26,29 @@ currency_code_vector<-c('AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 
                         'EGP', 'ERN', 'ETB', 'EUR',
                         'FJD', 'FKP',
                         'GBP', 'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD',
-                        'HKD', 'HNL', 'HRK', 'HTG', 'HUF', 
+                        'HKD', 'HNL', 'HRK', 'HTG', 'HUF',
                         'IDR', 'ILS', 'IMP', 'INR', 'IQD', 'IRR', 'ISK',
                         'JEP', 'JMD', 'JOD', 'JPY',
                         'KES', 'KGS', 'KHR', 'KMF', 'KPW', 'KRW', 'KWD', 'KYD', 'KZT',
                         'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LYD',
                         'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MRU', 'MUR', 'MVR', 'MWK', 'MXN', 'MYR', 'MZN',
-                        'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD', 
+                        'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD',
                         'OMR',
                         'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG',
                         'QAR',
                         'RON', 'RSD', 'RUB', 'RWF',
                         'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 'SLL', 'SOS', 'SRD', 'SSP', 'STD', 'STN', 'SVC', 'SYP', 'SZL',
                         'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD', 'TWD', 'TZS',
-                        'UAH', 'UGX', 'USD', 'UYU', 'UZS', 
-                        'VES', 'VND', 'VUV', 
-                        'WST', 
+                        'UAH', 'UGX', 'USD', 'UYU', 'UZS',
+                        'VES', 'VND', 'VUV',
+                        'WST',
                         'XAF', 'XAG', 'XAU', 'XCD', 'XDR', 'XOF', 'XPD', 'XPF', 'XPT',
                         'YER',
                         'ZAR', 'ZMW', 'ZWL')
 
-
+#' @export
 convert_currency <- function(from, to) {
-    
+
     if (from %in% currency_code_vector == FALSE) {
         warning("Invalid FROM currency name!")
         return ("Error")
@@ -57,8 +57,8 @@ convert_currency <- function(from, to) {
     if (to %in% currency_code_vector == FALSE) {
         warning("Invalid IN currency name!")
         return ("Error")
-        }    
-    
+        }
+
     url<-paste('https://api.exchangerate.host/convert?from=', from, '&to=', to, sep="")
     data <- fromJSON(url)
 
@@ -70,6 +70,6 @@ convert_currency <- function(from, to) {
     rate <- data$result
     date <- data$date
     output <- c(rate, date)
-    
+
     return (output)
 }
---
+++ tests/testthat/test_currency_time_series.R
@@ -0,0 +1,12 @@
+
+test_that("currency_time_series works", {
+  expect_equal(typeof(currency_time_series("2020-01-01","2020-02-03","CAD")),'list')
+  expect_equal(ncol(currency_time_series("2020-02-01","2020-02-03","CAD")),3)
+  expect_equal(currency_time_series("2020-01-01","2020-01-04","CAD")[[1]]$USD,0.77)
+  
+})
+
+test_that("currency_time_series throw errors and get warnings", {
+  expect_warning(currency_time_series("2020-01-01","2020-02-03","AD"))
+  expect_warning(currency_time_series(2020,"2020-02-03","CAD"))
+})
